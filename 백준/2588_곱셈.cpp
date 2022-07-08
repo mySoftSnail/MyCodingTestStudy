2a2b5b0d667dbac4c94db146095e42861c222cdd
Soojin Kim <snailking@kakao.com>
2022-07-08 18:18:05 +0900

    .

---
+++ 백준/2588_곱셈.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+
+	cout << A * (B % 10) << '\n';
+	cout << A * ((B / 10) % 10) << '\n';
+	cout << A * (B / 100) << '\n';
+	cout << A * B;
+
+	return 0;
+}
