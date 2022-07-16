f32bd9d6026b6963d190b3a3debb619e91e788ee
Soojin Kim <snailking@kakao.com>
2022-07-16 22:37:34 +0900

    .

---
+++ 백준/11022_A+B - 8.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	for(int i=1;i<=t;++i){
+		int a, b;
+		cin >> a >> b;
+
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+	}
+
+	return 0;
+}
