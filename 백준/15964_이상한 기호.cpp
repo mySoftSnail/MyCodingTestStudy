57d87e0ed6c2b2c8f6d2d34697cfa01d321c3e76
Soojin Kim <snailking@kakao.com>
2022-08-20 16:10:46 +0900

    220820

---
+++ 백준/15964_이상한 기호.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b;
+	cin >> a >> b;
+
+	cout << (a + b) * (a - b);
+
+	return 0;
+}
