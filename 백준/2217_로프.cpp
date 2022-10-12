fc9bd26b4c89737d8eb7bd3f7bb8b560775b407b
Soojin Kim <snailking@kakao.com>
2022-10-12 23:41:13 +0900

    221012

---
+++ 백준/2217_로프.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int w[100003];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+		cin >> w[i];
+
+	sort(w, w + n);
+
+	int ans = 0;
+	for (int i = 1; i <= n; ++i)
+		ans = max(ans, w[n - i] * i);
+	cout << ans;
+}
+
