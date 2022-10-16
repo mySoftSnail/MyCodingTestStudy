06dcd14a5b3ce56fd4314f149038f498dae8290f
Soojin Kim <snailking@kakao.com>
2022-10-16 20:57:43 +0900

    221016

---
+++ 백준/5522_카드 게임.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int sum = 0;
+	for (int i = 0; i < 5; ++i) {
+		int x;
+		cin >> x;
+		sum += x;
+	}
+	cout << sum;
+
+}
+
