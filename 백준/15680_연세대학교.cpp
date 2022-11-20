148be3b35f8225505bbf86c60eb509458dfc418f
Soojin Kim <snailking@kakao.com>
2022-11-20 22:07:17 +0900

    221120

---
+++ 백준/15680_연세대학교.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	if (n)
+		cout << "Leading the Way to the Future";
+	else
+		cout << "YONSEI";
+}
+
