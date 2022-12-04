49959472b75038a8ca9a0f8863b8f0057563913c
Soojin Kim <snailking@kakao.com>
2022-12-04 20:15:24 +0900

    221204

---
+++ 백준/7891_Can you add this.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int x, y;
+		cin >> x >> y;
+		cout << x + y << '\n';
+	}
+}
+
