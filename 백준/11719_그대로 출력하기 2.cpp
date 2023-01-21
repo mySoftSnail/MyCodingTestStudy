008bce2a31f717f68571ca464d2e6b1bc4f075c8
SoojinKim <snailking@kakao.com>
2023-01-21 19:36:58 +0900

    230121

---
+++ 백준/11719_그대로 출력하기 2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    string str;
+    while (!cin.eof()) {
+        getline(cin, str);
+        cout << str << '\n';
+    }
+}
